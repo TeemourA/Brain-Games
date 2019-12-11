797d0f619fbfd1ed3449759c3e796a5ee87f82cd
TeemourA <taxprogramming@gmail.com>
2019-12-12 00:19:09 +0300

    interaction added & few minor changes

--- src/index.js
+++ src/index.js
@@ -1,70 +1,59 @@
 import readlineSync from 'readline-sync';
 
 const print = (text, name = '') => {
-  console.log(`${text}, ${name}!`);
+  console.log(`${text}${name}!`);
 };
 
-export const generateNumber = (n = 1, m = 0) => m + Math.floor(Math.random() * n);
+const generateNumber = (n = 1, m = 0) => m + Math.floor(Math.random() * n);
 
-export const getFirstParameter = (f) => f((x) => x);
+const getFirstParameter = (f) => f((x) => x);
 
-export const getSecondParameter = (f) => f((x, y) => y);
+const getSecondParameter = (f) => f((x, y) => y);
 
-export const getThirdParameter = (f) => f((x, y, z) => z);
-
-export const askName = (quiz = '') => {
-  const greeting = () => {
-    console.log('Welcome to the Brain Games!');
-  };
-
-  const getName = () => {
-    const name = readlineSync.question('May I have your name?');
-    const anon = 'Anonymous';
-    return name.length ? name : anon;
-  };
-
-  switch (quiz) {
-    case 'brainEven':
-      greeting();
-      console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
-      break;
-    case 'brainCalc':
-      greeting();
-      console.log('What is the result of the expression?\n');
-      break;
-    case 'brainGCD':
-      greeting();
-      console.log('Find the greatest common divisor of given numbers.\n');
-      break;
-    case 'brainProgression':
-      greeting();
-      console.log('What number is missing in the progression?\n');
-      break;
-    case 'brainPrime':
-      greeting();
-      console.log('Answer "yes" if given number is prime. Otherwise answer "no".\n');
-      break;
-    default:
-      greeting();
-  }
-  const name = getName();
-  print('Hello', name);
-  return name;
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
 };
 
-export const playQuiz = (quiz, name, counter = 0) => {
-  if (counter === 3) {
-    console.log(`Congratulations, ${name}!`);
-  } else {
-    const result = quiz(name);
-    const rightAnswer = getFirstParameter(result);
-    const currentAnswer = getSecondParameter(result);
-    const retry = getThirdParameter(result);
+const showRules = (rules) => {
+  console.log(`${rules}\n`);
+};
 
-    if (rightAnswer !== currentAnswer) {
-      retry();
+const getName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  const anon = 'Anonymous';
+  return name.length ? name : anon;
+};
+
+const retry = (userName, rightAnswer, currentAnswer) => console.log(`Let's try again, ${userName}!\n'${currentAnswer}' is wrong answer ;(. Correct answer is '${rightAnswer}'.`);
+
+const playQuiz = (quiz, rules) => {
+  greeting();
+  showRules(rules);
+  const userName = getName();
+  print('Hello, ', userName);
+  const iter = (quizFlow, name, counter = 0) => {
+    if (counter === 3) {
+      print('Congratulations, ', name);
     } else {
-      playQuiz(quiz, name, counter + 1);
+      const result = quizFlow();
+      const question = getFirstParameter(result);
+      const rightAnswer = getSecondParameter(result);
+      console.log(`Question: ${question}`);
+      const currentAnswer = readlineSync.question('Your answer: ');
+
+      if (rightAnswer !== currentAnswer) {
+        retry(name, rightAnswer, currentAnswer);
+      } else {
+        iter(quizFlow, name, counter + 1);
+      }
     }
-  }
+  };
+  return iter(quiz, userName);
 };
+
+export {
+  generateNumber, getFirstParameter, getSecondParameter, playQuiz,
+};
+
+// eslint-disable-next-line max-len
+// export const getThirdParameter = (f) => f((x, y, z) => z); --- to get extra data from quizFlow if needed
