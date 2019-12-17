cefb08f269c087ed292298f11e4b938372d85487
TeemourA <taxprogramming@gmail.com>
2019-12-17 22:50:54 +0300

    new module - add number generator utility

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+const generateNumber = (minimum, maximum) => {
+  const min = Math.ceil(minimum);
+  const max = Math.floor(maximum);
+  return Math.floor(Math.random() * (max - min + 1)) + min; // both inclusive
+};
+
+export default generateNumber;
