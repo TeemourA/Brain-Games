80bf3fc719be186801dfc48529f2618090074792
TeemourA <taxprogramming@gmail.com>
2019-12-24 21:52:19 +0300

    refactor

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,6 +3,7 @@ import generateNumber from '../utils';
 import playGame from '..';
 
 const rule = 'What number is missing in the progression?';
+const progressionLength = 10;
 
 const makeProgression = (base, increment, length, progression) => {
   if (length) {
@@ -12,14 +13,12 @@ const makeProgression = (base, increment, length, progression) => {
   return progression;
 };
 
-
 const generateBrainProgressionData = () => {
   const base = generateNumber(1, 5);
   const increment = generateNumber(2, 5);
-  const length = 10;
-  const secretValueIndex = generateNumber(1, length);
+  const secretValueIndex = generateNumber(1, progressionLength);
 
-  const progression = makeProgression(base, increment, length, '');
+  const progression = makeProgression(base, increment, progressionLength, '');
 
   const rightAnswer = (base + (increment * secretValueIndex)).toString();
   const currentQuestion = progression.replace(rightAnswer, '..');
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from '@hexlet/pairs';
 
-const getName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  const anon = 'Anonymous';
-  return name.length ? name : anon;
-};
+const roundsCount = 3;
+const isAnonymous = (name) => (!name.length);
 
 const playQuiz = (generateGameData, rule) => {
-  const roundsCount = 3;
   console.log('Welcome to the Brain Games!');
   console.log(`${rule}\n`);
-  const userName = getName();
+  const nameValue = readlineSync.question('May I have your name? ');
+  const userName = isAnonymous(nameValue) ? 'Anonymous' : nameValue;
   console.log(`Hello, ${userName}!`);
 
   const iter = (roundCounter) => {
@@ -25,10 +22,12 @@ const playQuiz = (generateGameData, rule) => {
       const userAnswer = readlineSync.question('Your answer: ');
 
       if (userAnswer !== rightAnswer) {
-        console.log(`Let's try again, ${userName}!\n'${userAnswer}' is wrong answer ;(. Correct answer is '${rightAnswer}'.`);
-      } else {
-        iter(roundCounter - 1);
+        console.log(`Let's try again, ${userName}!`);
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer is '${rightAnswer}'.`);
+
+        return;
       }
+      iter(roundCounter - 1);
     }
   };
   iter(roundsCount);
